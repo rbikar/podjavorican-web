655f1f73bf0e2c9bc79d3b9257ad65fae9f9f437
Robert Bikar <rbikar@redhat.com>
2026-01-02 21:04:13 +0100

    Added jekyll exaple site
    * site is properly server via flask app
    * minor updates

--- flask-app/app.py
+++ flask-app/app.py
@@ -1,17 +1,24 @@
 from flask import Flask, send_from_directory
-
-STATIC_FOLDER = '../public'
+import os
+STATIC_FOLDER = '../public/'
 
 app = Flask(__name__, static_folder=STATIC_FOLDER)
 
 @app.route('/', defaults={'path': 'index.html'})
-@app.route('/<path:path>')
+@app.route('/<path:path>',)
 def serve_static(path):
-    return send_from_directory('../public', path)
+    print(f'serving {path}')
+    return send_from_directory(STATIC_FOLDER, path)
 
 @app.route('/api/data')
 def dynamic():
     return {'data': 'From Flask backend'}
+
+@app.errorhandler(404)
+def page_not_found(e):
+    # note that we set the 404 status explicitly
+    return send_from_directory(STATIC_FOLDER, '404.html'), 404
+
 
 if __name__ == '__main__':
     app.run(debug=True) # TODO debug via env. var
