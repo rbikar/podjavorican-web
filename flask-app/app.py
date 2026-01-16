65f2e2ae8f9faa423e6d025ee9d6f96ae9ed1be8
Robert Bikar <rbikar@redhat.com>
2026-01-16 22:39:39 +0100

    Total reset
    - kept only flask minimal app, will be moved to another repo
    - started web with Hugo SSG
    - functional part of HomePage

--- flask-app/app.py
+++ flask-app/app.py
@@ -1,15 +1,19 @@
 from flask import Flask, send_from_directory
 import os
-STATIC_FOLDER = '../public/'
+SITE_DIR = os.path.abspath('public')
 
-app = Flask(__name__, static_folder=STATIC_FOLDER)
+app = Flask(__name__, static_folder=SITE_DIR)
 
 @app.route('/', defaults={'path': 'index.html'})
 @app.route('/<path:path>',)
 def serve_static(path):
     print(f'serving {path}')
-    return send_from_directory(STATIC_FOLDER, path)
+    full_path = os.path.join(SITE_DIR, path)
+         
+    if os.path.isdir(full_path):
+        path = os.path.join(path, 'index.html')
 
+    return send_from_directory(SITE_DIR, path)
 @app.route('/api/data')
 def dynamic():
     return {'data': 'From Flask backend'}
@@ -17,7 +21,7 @@ def dynamic():
 @app.errorhandler(404)
 def page_not_found(e):
     # note that we set the 404 status explicitly
-    return send_from_directory(STATIC_FOLDER, '404.html'), 404
+    return send_from_directory(SITE_DIR, '404.html'), 404
 
 
 if __name__ == '__main__':
